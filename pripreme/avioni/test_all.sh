b372cbcc33c1a17a23eafb4236953aa324e202ec
Filip Pavetic <filip@skillset.me>
2014-05-26 17:00:39 +0200

    test_all.sh fix

--- pripreme/avioni/test_all.sh
+++ pripreme/avioni/test_all.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-g++ -o prepare_submission prepare_submission.cpp
-./prepare_submission jaccard.cpp >submission.cpp
-g++ -o submission submission.cpp -O2
+mkdir bin
+mkdir tmp
+g++ -o bin/prepare_submission prepare_submission.cpp
+bin/prepare_submission jaccard.cpp >tmp/submission.cpp
+g++ -o bin/submission tmp/submission.cpp -O2
 
 for i in `ls test_data`;
 do
     echo $i
-    cat test_data/$i | gunzip | ./submission
-done > results
+    cat test_data/$i | gunzip | bin/submission
+done > tmp/results
 
-g++ -o stats stats.cpp
-./stats <results
+g++ -o bin/stats stats.cpp
+bin/stats <tmp/results
+rm -r bin
+rm -r tmp
